7b3fd319dea52ee05c9fe576938acba11e9afc97
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2025-12-11 07:32:21 +0100

    feat: extend Keys module with global key support
    - Add new key types: PageUp, PageDown, Home, End, Escape, Delete, Function
    - Define global_key type for app-wide actions (Settings, Help, Menu, Quit)
    - Add is_global_key/get_global_action/show_global_keys functions
    - Update of_string/to_string/to_label to handle new keys
    
    This lays the foundation for compile-time key conflict detection.

--- src/miaou_core/keys.mli
+++ src/miaou_core/keys.mli
@@ -17,6 +17,16 @@ type t =
   | Backspace
   | Char of string
   | Control of string
+  | PageUp
+  | PageDown
+  | Home
+  | End
+  | Escape
+  | Delete
+  | Function of int
+
+(* Global keys reserved for application-wide functionality *)
+type global_key = Settings | Help | Menu | Quit
 
 val of_string : string -> t option
 
@@ -25,3 +35,12 @@ val to_string : t -> string
 val equal : t -> t -> bool
 
 val to_label : t -> string
+
+(* Check if a key is reserved as a global key *)
+val is_global_key : t -> bool
+
+(* Get the global action for a key string *)
+val get_global_action : string -> global_key option
+
+(* Get list of global key bindings for display *)
+val show_global_keys : unit -> (string * string) list
